00120dea18a1e17903f96262b537c9cb34280449
Mark Mwima <markmwimar@gmail.com>
2024-09-11 12:15:42 +0300

    response body annotation

--- src/main/java/com/mwimar/springboot_rest/JobRestController.java
+++ src/main/java/com/mwimar/springboot_rest/JobRestController.java
@@ -5,6 +5,7 @@ import com.mwimar.springboot_rest.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -14,6 +15,7 @@ public class JobRestController {
     private JobService service;
 
     @GetMapping("jobPosts")
+    @ResponseBody
     public List<JobPost> getAllJobs(){
         return service.getAllJobs();
     }
