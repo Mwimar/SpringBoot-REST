9bfb4ee14a704f85f86660b49259d7b64379de36
Mark Mwima <markmwimar@gmail.com>
2024-09-11 12:02:38 +0300

    autowire job service

--- src/main/java/com/mwimar/springboot_rest/JobRestController.java
+++ src/main/java/com/mwimar/springboot_rest/JobRestController.java
@@ -1,7 +1,14 @@
 package com.mwimar.springboot_rest;
 
+import com.mwimar.springboot_rest.service.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class JobRestController {
+    @Autowired
+    private JobService service;
+    public String getAllJobs(){
+
+    }
 }
