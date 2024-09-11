2d6269457198b7c9b836079d80fc3d027438c3ef
Mark Mwima <markmwimar@gmail.com>
2024-09-11 12:50:33 +0300

    allowing crossorigin

--- src/main/java/com/mwimar/springboot_rest/JobRestController.java
+++ src/main/java/com/mwimar/springboot_rest/JobRestController.java
@@ -4,6 +4,7 @@ import com.mwimar.springboot_rest.model.JobPost;
 import com.mwimar.springboot_rest.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins="http://localhost:3000")
 public class JobRestController {
     @Autowired
     private JobService service;
