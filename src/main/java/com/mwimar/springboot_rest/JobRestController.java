bbea237262bd848f5654ababd69ba64d04955398
Mark Mwima <markmwimar@gmail.com>
2024-09-11 12:04:57 +0300

    returning job post list

--- src/main/java/com/mwimar/springboot_rest/JobRestController.java
+++ src/main/java/com/mwimar/springboot_rest/JobRestController.java
@@ -1,14 +1,18 @@
 package com.mwimar.springboot_rest;
 
+import com.mwimar.springboot_rest.model.JobPost;
 import com.mwimar.springboot_rest.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import java.util.List;
 
 @Controller
 public class JobRestController {
     @Autowired
     private JobService service;
-    public String getAllJobs(){
 
+    public List<JobPost> getAllJobs(){
+        return service.getAllJobs();
     }
 }
