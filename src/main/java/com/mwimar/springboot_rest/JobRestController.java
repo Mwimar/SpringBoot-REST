6770361d05202c115360ee8a99cad7935ea7be82
Mark Mwima <markmwimar@gmail.com>
2024-09-11 12:21:04 +0300

    rest controller annotation

--- src/main/java/com/mwimar/springboot_rest/JobRestController.java
+++ src/main/java/com/mwimar/springboot_rest/JobRestController.java
@@ -6,16 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class JobRestController {
     @Autowired
     private JobService service;
 
     @GetMapping("jobPosts")
-    @ResponseBody
     public List<JobPost> getAllJobs(){
         return service.getAllJobs();
     }
