ec87a90aaf01ffcf52b484a162aae1b8dfe02176
Mark Mwima <markmwimar@gmail.com>
2024-09-11 12:06:19 +0300

    mapping all jobs

--- src/main/java/com/mwimar/springboot_rest/JobRestController.java
+++ src/main/java/com/mwimar/springboot_rest/JobRestController.java
@@ -4,6 +4,7 @@ import com.mwimar.springboot_rest.model.JobPost;
 import com.mwimar.springboot_rest.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
@@ -12,6 +13,7 @@ public class JobRestController {
     @Autowired
     private JobService service;
 
+    @GetMapping("jobPosts")
     public List<JobPost> getAllJobs(){
         return service.getAllJobs();
     }
